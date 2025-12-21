26a398cd1189c8afb40e3e2222b4201f64dfbb55
RajputVikashS5 <vikashrajput935843@gmail.com>
2025-12-22 04:10:41 +0530

    fix issue of product card

--- src/models/Wishlist.js
+++ src/models/Wishlist.js
@@ -4,8 +4,7 @@ const WishlistSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true,
-    index: true
+    required: true
   },
   items: [
     {
